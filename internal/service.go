30344456e5f86aeb25b5c36bcc2d29f02005f8f6
Jake Pelletier <ghjnut@gmail.com>
2023-03-07 22:26:03 -0700

    split create logs into call to createlog

--- internal/service.go
+++ internal/service.go
@@ -7,9 +7,19 @@ import (
 )
 
 type Service interface {
+	CreateLog(context.Context, CreateLogIn) (CreateLogOut, error)
 	CreateLogs(context.Context, CreateLogsIn) (CreateLogsOut, error)
 	Logs(context.Context, LogsIn) ([]sensor.Log, error)
 	Device(context.Context, DeviceIn) (sensor.Device, error)
+}
+
+type CreateLogIn struct {
+	EventDate string
+	DeviceID  string
+	TempF     string
+}
+
+type CreateLogOut struct {
 }
 
 type CreateLogsIn struct {
--- internal/service/service.go
+++ internal/service/service.go
@@ -20,39 +20,37 @@ func New(db *sql.DB) internal.Service {
 	return &service{db: db}
 }
 
-// TODO should we instead be calling CreateLog multiple times?
-//func (s *service) CreateLog(context.Context, CreateLogIn) (CreateLogOut, error) {
-//}
+func (s *service) CreateLog(ctx context.Context, in internal.CreateLogIn) (out internal.CreateLogOut, err error) {
+	//2022-03-28T18:55:35.433Z
+	t, err := time.Parse(time.RFC3339, in.EventDate)
+	if err != nil {
+		return out, err
+	}
+
+	//`TempF = (2*TempC) + 30`
+	temp_c, err := strconv.Atoi(in.TempF)
+	if err != nil {
+		return out, err
+	}
+	temp_f := 2*temp_c + 30
+
+	_, err = s.db.Exec("INSERT INTO logs (event_date, device_id, temp_farenheit) VALUES ($1, $2, $3)", t, in.DeviceID, temp_f)
+	return out, err
+}
 
 // TODO wrap in transaction and rollback
-// TODO CreateLogsIn should be a struct with the 3 variables
 func (s *service) CreateLogs(ctx context.Context, in internal.CreateLogsIn) (out internal.CreateLogsOut, err error) {
 	for i := 0; i < len(in.Data); i++ {
 		fields := strings.Split(in.Data[i], "|")
 		if len(fields) != 3 {
 			return out, errors.New("bad payload")
 		}
-
-		//2022-03-28T18:55:35.433Z
-		t, err := time.Parse(time.RFC3339, fields[1])
-		if err != nil {
-			return out, err
+		clin := internal.CreateLogIn{
+			DeviceID:  fields[0],
+			EventDate: fields[1],
+			TempF:     fields[2],
 		}
-
-		//`TempF = (2*TempC) + 30`
-		temp_c, err := strconv.Atoi(fields[2])
-		if err != nil {
-			return out, err
-		}
-		temp_f := 2*temp_c + 30
-
-		//l := &Log{
-		//  DeviceID:     fields[0],
-		//  Date:         t,
-		//  TemperatureF: temp_f,
-		//}
-		//_, err := db.Exec("INSERT INTO logs (event_date, device_id, temp_farenheit) VALUES ($1, $2, $3)", l.Date, l.DeviceID, l.TemperatureF)
-		_, err = s.db.Exec("INSERT INTO logs (event_date, device_id, temp_farenheit) VALUES ($1, $2, $3)", t, fields[0], temp_f)
+		_, err = s.CreateLog(ctx, clin)
 		if err != nil {
 			return out, err
 		}
